d10b5ab584f9d3f3eef1d5c21aa85cfe55e611b1
pibyi <pibyi09@gmail.com>
2026-01-03 20:41:35 +0600

    fix: add z-index to header for improved stacking context

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Header = ({ products }) => {
     return (
-        <header className="bg-nomadory-background pt-12 pb-6 px-4 md:px-6 md:pb-12 flex items-center justify-between sticky h-[var(--nomadory-header-height)] top-0">
+        <header className="bg-nomadory-background pt-12 pb-6 px-4 md:px-6 md:pb-12 flex items-center justify-between sticky h-[var(--nomadory-header-height)] top-0 z-50">
             <Link href="/">
                 <LogoFull />
             </Link>
