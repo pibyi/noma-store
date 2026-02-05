6d01dfdf005e1890dd9ed60307f3dd9d3813a04a
pibyi <pibyi09@gmail.com>
2026-02-05 16:54:46 +0600

    refactor: move `CraftedSpaces` component to appear after `HeroSection` on the homepage.

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -17,11 +17,11 @@ const HomePage = async () => {
             <div className="h-20 md:h-40" />
             <HeroSection />
             <div className="h-20 md:h-40" />
+            <CraftedSpaces />
+            <div className="h-20 md:h-40" />
             <ProductGallery />
             <div className="h-20 md:h-40" />
             <RugsStory />
-            <div className="h-20 md:h-40" />
-            <CraftedSpaces />
             <div className="h-20 md:h-40" />
             <ExploreOpportunities />
             <div className="h-20 md:h-40" />
